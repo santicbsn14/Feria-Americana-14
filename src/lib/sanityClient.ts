63e0910187fe22d76376506bb999e992d5131d89
santiago_viale <santicbsn9@gmail.com>
2026-05-14 23:38:14 -0300

    perfomance.1

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,12 +1,11 @@
 import { createClient } from '@sanity/client'
 
 export const sanityClient = createClient({
-  projectId: 'ybsbntnx',
+  projectId: 'airq7lny',
   dataset: 'production',
   useCdn: true,
   apiVersion: '2024-01-01',
 })
-
 export function imgUrl(url: string, width: number = 800): string {
   if (!url) return ''
   return `${url}?auto=format&q=80&w=${width}&fit=max`
